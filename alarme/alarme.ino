ff76b835bbed770de75027a180e2e43470700769
Rafael Strack <strackinado@gmail.com>
2023-03-23 20:15:12 -0300

    mudança para Firebase RTDB e finalização de configurações

--- alarme/alarme.ino
+++ alarme/alarme.ino
@@ -1,3 +1,5 @@
+#include <WifiConfig.h>
+
 #if defined(ESP32) || defined(PICO_RP2040)
 #include <WiFi.h>
 #elif defined(ESP8266)
@@ -10,25 +12,36 @@
 
 #include <addons/TokenHelper.h>
 
-#define API_KEY "AIzaSyCr1m4GZ9pY9oAF-FDEOkKEfJjc2ONydOM"
-#define FIREBASE_PROJECT_ID "dgr-alarmes"
+//lib com dados sensíveis
+#include <WifiConfig.h>
 
-#define USER_EMAIL "root@root.com"
-#define USER_PASSWORD "root123"
+#define DATABASE_URL "https://dgr-alarmes-default-rtdb.firebaseio.com/"
 
 #define INI_FILE "/net.ini"
 
 FirebaseData fbdo;
+FirebaseData stream;
 
 FirebaseAuth auth;
 FirebaseConfig config;
 
 String macAddress;
 
+bool activeAlarm;
+bool triggeredAlarm = false;
+
+//----- alarm vars -----
+int freq = 500;
+int _time = 10;
+bool up = true;
+
 // -------------------- SETUP --------------------
 
 void setup(){
   Serial.begin(115200);
+
+  pinMode(5, INPUT);
+  pinMode(4, OUTPUT);
 
   if (!SPIFFS.begin())
     while (1)
@@ -51,56 +64,46 @@ void setup(){
     Serial.println("Conectado ao Wi-fi com sucesso!");
   }
 
-  config.api_key = API_KEY;
-  auth.user.email = USER_EMAIL;
-  auth.user.password = USER_PASSWORD;
+  firebaseConfig();
 
-  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+  if (!Firebase.RTDB.beginStream(&stream, "/device/"+macAddress+""))
+    Serial.printf("stream begin error, %s\n\n", stream.errorReason().c_str());
 
-  #if defined(ESP8266)
-    // In ESP8266 required for BearSSL rx/tx buffer for large data handle, increase Rx size as needed.
-    fbdo.setBSSLBufferSize(2048 /* Rx buffer size in bytes from 512 - 16384 */, 2048 /* Tx buffer size in bytes from 512 - 16384 */);
-  #endif
+  if(Firebase.RTDB.getBool(&fbdo, "/device/"+macAddress+"/active"))
+    activeAlarm = fbdo.to<bool>();
+  else activeAlarm = false;
 
-    // Limit the size of response payload to be collected in FirebaseData
-  fbdo.setResponseSize(2048);
+  if(!Firebase.RTDB.setBool(&fbdo, "/device/"+macAddress+"/triggered", false))
+    Serial.println(fbdo.errorReason().c_str());
 
-  Firebase.begin(&config, &auth);
-
-  Firebase.reconnectWiFi(true);
-
-  String documentPath = "device/"+macAddress+"";
-  //String mask = WiFi.macAddress();
-  //String mask = "teste";
-
-  if (Firebase.ready()){
-    if (!Firebase.Firestore.getDocument(&fbdo, FIREBASE_PROJECT_ID, "", documentPath.c_str())){
-      Serial.println("Dispositivo não encontrado no firebase, criando documento...");
-      FirebaseJson content;
-      String documentPath = "device/"+macAddress+"";
-      content.set("fields/active/booleanValue", false);
-      content.set("fields/triggered/booleanValue", false);
-      if (Firebase.Firestore.createDocument(&fbdo, FIREBASE_PROJECT_ID, "", documentPath.c_str(), content.raw()))
-        Serial.printf("Dispositivo Criado\n%s\n", fbdo.payload().c_str());
-      else
-        Serial.println(fbdo.errorReason());
-      }
-    else
-      Serial.println(fbdo.errorReason());
-  }
-  else{
-    Serial.println("Problema na conexão com Firebase.");  
-  }
+  //delay para o comando acima não ser reconhecido pelo stream
+  delay(200);
+  
+  Firebase.RTDB.setStreamCallback(&stream, streamCallback, streamTimeoutCallback);
 }
 
 // -------------------- LOOP --------------------
 
 void loop(){
-
+  //lógica só é aceita se o alarme estiver ligado
+  if(activeAlarm){
+    //se alarme estiver disparado, emite som
+    if(triggeredAlarm){
+      alarm();  
+    }
+    //verifica sensor de proximidade e dispara o alarme
+    if(digitalRead(5)){
+      if(!triggeredAlarm){
+        Firebase.RTDB.setBool(&fbdo, "device/"+macAddress+"/triggered", true);
+      }
+    }
+  }else{
+    if(triggeredAlarm)
+      Firebase.RTDB.setBool(&fbdo, "device/"+macAddress+"/triggered", false);
+  }
 }
 
-// -------------------- FUNCTIONS --------------------
-
+// -------------------- FUNÇÕES --------------------
 File iniFile(){
   File file = SPIFFS.open(INI_FILE, "r");
   if (!file) {
@@ -111,7 +114,7 @@ File iniFile(){
 
     File file = SPIFFS.open(INI_FILE, "w");
     //hard code
-    int bytesWritten = file.print("ALHN-D2D9 2.4G\nPwbDv7N=ay");
+    int bytesWritten = file.print(String(WIFI_SSID) + "\n" + String(WIFI_PASSWORD));
     if (bytesWritten > 0) {
       Serial.println("Arquivo foi escrito com sucesso");
       Serial.println(bytesWritten);
@@ -142,4 +145,104 @@ bool testWifi(void)
   Serial.println("");
   Serial.println("Tempo para conexão esgotado");
   return false;
+}
+
+void firebaseConfig(){
+  config.api_key = API_KEY;
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+  config.database_url = DATABASE_URL;
+
+  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+
+  #if defined(ESP8266)
+    // In ESP8266 required for BearSSL rx/tx buffer for large data handle, increase Rx size as needed.
+    fbdo.setBSSLBufferSize(2048 /* Rx buffer size in bytes from 512 - 16384 */, 2048 /* Tx buffer size in bytes from 512 - 16384 */);
+    stream.setBSSLBufferSize(2048 /* Rx in bytes, 512 - 16384 */, 512 /* Tx in bytes, 512 - 16384 */);
+  #endif
+
+  // Limit the size of response payload to be collected in FirebaseData
+  fbdo.setResponseSize(2048);
+
+  Firebase.begin(&config, &auth);
+
+  Firebase.reconnectWiFi(true);
+
+  String documentPath = "device/"+macAddress+"";
+  //String mask = WiFi.macAddress();
+  //String mask = "teste";
+
+  if (Firebase.ready()){
+    if (!Firebase.RTDB.get(&fbdo, documentPath.c_str())){
+      Serial.println("Dispositivo não encontrado no firebase, criando documento...");
+      String documentPath = "device/"+macAddress+"";
+      FirebaseJson content;
+      content.set("active", false);
+      content.set("triggered", false);
+      if (Firebase.RTDB.set(&fbdo, documentPath, &content))
+        Serial.printf("Dispositivo Criado\n%s\n", fbdo.payload().c_str());
+      else
+        Serial.println(fbdo.errorReason());
+      }
+    else
+      Serial.println("Dispositivo ja existente no Firebase");
+  }
+  else{
+    Serial.println("Problema na conexão com Firebase.");  
+  }
+}
+
+void streamCallback(FirebaseStream data){
+  if (data.dataType() == "null") {
+    Serial.println("No data available");
+  }
+  else{
+    Serial.println("Atualização recebida");
+    Serial.println(data.dataPath());
+    if(data.dataPath()=="/active"){
+      activeAlarm = !activeAlarm;
+      Serial.print("Alarme ativo: ");
+      Serial.println(activeAlarm);
+    }else if (data.dataPath()=="/triggered"){
+      triggeredAlarm = !triggeredAlarm;
+      Serial.print("Alarme disparado: ");
+      Serial.println(triggeredAlarm);
+      freq=500;
+      up = true;
+    }
+  }
+}
+
+void streamTimeoutCallback(bool timeout)
+{
+  if (timeout)
+    Serial.println("stream timed out, resuming...\n");
+
+  if (!stream.httpConnected())
+    Serial.printf("error code: %d, reason: %s\n\n", stream.httpCode(), stream.errorReason().c_str());
+}
+
+void alarm(){
+  if (up && freq < 1800){
+    tone(4, freq, _time);
+    delay(_time);
+    freq = freq + 100;
+  }
+  else if(up && freq == 1800){
+    up = false;
+    tone(4, freq, _time);
+    delay(_time);
+    freq = freq - 100;
+  }
+  else if (!up && freq==500){
+    up = true;
+    tone(4, freq, _time);
+    delay(_time);
+    freq = freq + 100;
+  }
+  else{
+    tone(4, freq, _time);
+    delay(_time);
+    freq = freq - 100;
+  }
 }
