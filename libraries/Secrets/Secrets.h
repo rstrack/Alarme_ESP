a5b0f2e1a5880c03e64a509260be8dc8d79dfbbd
Rafael Strack <strackinado@gmail.com>
2023-05-08 22:46:34 -0300

    adição do arquivo secrets.h para armazenar dados sensíveis

---
+++ libraries/Secrets/Secrets.h
@@ -0,0 +1,11 @@
+#ifndef SECRETS_H_INCLUDED
+#define SECRETS_H_INCLUDED
+
+#pragma once
+
+#define API_KEY "API_KEY"
+#define USER_EMAIL "email@email.com"
+#define USER_PASSWORD "psswd123"
+#define FIREBASE_PROJECT_ID "PROJECT_ID"
+
+#endif // SECRETS_H_INCLUDED
